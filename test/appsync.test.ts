5863f79dc8f8882c9aeb2a5fea76b3743f6b9e90
David Kagerer <david@binder-kagerer.de>
2020-03-21 21:12:53 +0100

    🎉initial commit

--- test/appsync.test.ts
+++ test/appsync.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import Appsync = require('../lib/appsync-stack');
+import AppSyncStack = require('../lib/appsync-stack');
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new Appsync.AppsyncStack(app, 'MyTestStack');
+    const stack = new AppSyncStack.AppSyncStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
